8ec5d2f335ea7ca11435f746e3e0d25e4e8d95f1
Genryu <buhisanrheymark@gmail.com>
2025-11-29 03:41:21 +0800

    fixed layout issue in modals p2

--- resources/views/components/modal.blade.php
+++ resources/views/components/modal.blade.php
@@ -24,7 +24,7 @@
 
                 <div class="flex flex-row justify-between items-center px-6 py-4 space-x-1">
 
-                    <div class="text-[14px] flex flex-row justify-center items-center gap-2 transition duration-150 w-1/2">
+                    <div class="text-[14px] flex flex-row justify-start items-center gap-2 transition duration-150 w-1/2">
                         {{ $modal_info ?? '' }}
                     </div>
                     <div class="flex flex-row justify-end items-center gap-2 w-1/2">
